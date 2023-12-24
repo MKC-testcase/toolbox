efcba686c4f41775070cf3863f4835735fe27b0d
Marcus Chan <chanmarcus53@gmail.com>
2023-12-23 23:59:44 -0800

    Initial sql alchemy framework

--- sql_alchemy_connector.py
+++ sql_alchemy_connector.py
@@ -1,1 +1,27 @@
 from sqlalchemy import Table, Float, Integer, String, Boolean
+
+### The purpose of this project is to use sqlalchemy library to handle all my database
+# requirements for future projects
+
+class sql_alch:
+    def __init__(self, database_type, username, password, host_server, database_name):
+        match database_type.lower:
+            case "postgres":
+                self.database = "postgresql+psycopg2"
+            case "mysql":
+                self.database = "mysql+mysqlconnector"
+            case "mssql":
+                self.database = "mssql+pyodbc"
+        self.connection_string = f"{self.database}://{username}:{password}@{host_server}/{database_name}"
+
+    def start_connection(self):
+        pass
+
+    def query(self, select, table, limits, **kwargs):
+        pass
+
+    def insert(self, table, values):
+        pass
+
+    def close(self):
+        pass
